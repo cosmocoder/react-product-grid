3d7f7f63f8ca1a8b8730e5ed244ecfd7da0436c2
Nilok Bose <cosmocoder@gmail.com>
2016-06-06 23:29:36 +0530

    Slight edit to spacing for better readability

--- static/index.js
+++ static/index.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import {MainContainer} from './containers'
+import { MainContainer } from './containers'
 import './styles/style.scss'
 
-ReactDOM.render(<MainContainer/>, document.getElementById('products-app'))
+ReactDOM.render(<MainContainer />, document.getElementById('products-app'))
